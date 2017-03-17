4637d0bde1e41d56bd96aed0051f1c84dfe2e0ca
Елена <elena.fatya@yandex.ru>
2017-03-17 16:59:53 +0300

    add new files and start work on the library

---
+++ js/synch-tabs-1.0.js
@@ -0,0 +1,26 @@
+;
+(function(window, undefined){
+    'use strict';
+    function WorkTabClass() {
+
+
+        this.currentTab = () => {
+
+        };
+        this.setActiveTab = (num) => {
+
+        };
+        this.queueTabs = () => {
+
+        };
+        this.onTabActivated = () => {
+
+        };
+    }
+
+    var synchTabs = () => {
+        var thisTab = new WorkTabClass();
+        return true;
+    };
+    window.synchTabs = synchTabs;
+})(window);
