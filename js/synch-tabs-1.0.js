c8d27fd93b809003012e2852aac242c821951775
Елена <elena.fatya@yandex.ru>
2017-03-21 20:51:34 +0300

    add  save tab and queue tabs

--- js/synch-tabs-1.0.js
+++ js/synch-tabs-1.0.js
@@ -60,6 +60,53 @@
 
         }
     }
+    function clearInset(){
+        var arrIdInsets = JSON.parse(localStorage.getItem('arrIdInsets'));
+        arrIdInsets.forEach(function(elem, i){
+            if(parseInt(elem) == parseInt(sessionStorage.getItem('playerInset'))){
+                arrIdInsets.splice(i, 1);
+                return false;
+            }
+        });
+        localStorage.setItem('arrIdInsets', JSON.stringify(arrIdInsets));
+        if(('iPlay' in auth)){
+            if(arrIdInsets.length > 0){
+                delete auth.iPlay;
+                localStorage.setItem('tabActive', arrIdInsets.slice(0, 1)[0]);
+            }else{
+                delete window.localStorage.arrIdInsets;
+                delete window.localStorage.tabActive;
+            }
+        }
+        return arrIdInsets;
+    }
+
+    //$(window).on('beforeunload', function(event) {
+    //    var arrIdInsets = JSON.parse(localStorage.getItem('arrIdInsets'));
+    //    if(('videoJsPlayer' in auth) && ('iPlay' in auth) && (arrIdInsets.length > 1)){
+    //        auth.videoJsPlayer.pause();
+    //        $('.b-progress-line-full').css({width: '0%'});
+    //    }
+    //    arrIdInsets = clearInset();
+    //    if(arrIdInsets == null){
+    //        arrIdInsets = [];
+    //    }
+    //
+    //    $('body').on('mouseenter', function () {
+    //        $('body').unbind('mouseenter');
+    //        setTimeout(function(){
+    //            if(arrIdInsets.length > 0){
+    //                determineDevice();
+    //            }else{
+    //                //add in arr
+    //                arrIdInsets.push(parseInt(sessionStorage.getItem('playerInset')));
+    //                localStorage.setItem('arrIdInsets', JSON.stringify(arrIdInsets));
+    //            }
+    //            console.log('queue tabs: ' + localStorage.getItem('arrIdInsets'));
+    //        },3000);
+    //    });
+    //    return true;
+    //});
 
     var synchTabs = () => {
         window.addEventListener('storage', storageEventHandler);
